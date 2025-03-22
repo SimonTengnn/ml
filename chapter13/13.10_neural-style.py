a1cdd9423a8097e05699f07ed4c076aa993c9337
Simon <1371121142@qq.com>
2025-03-22 15:59:21 +0800

    refine: 13.10 2025/03/22

--- chapter13/13.10_neural-style.py
+++ chapter13/13.10_neural-style.py
@@ -64,5 +64,68 @@ def get_styles(image_shape, device):
     _, style_Y = extract_features(style_X, content_layers, style_layers)
     return style_X, style_Y
 
+def content_loss(Y_hat, Y):
+    return torch.square(Y_hat - Y.detach()).mean() # Y.detach()生成一个新的tensor用于计算，但是不计算梯度（切断Y与计算图的联系）
+    # .mean()得到一个标量
+
+def gram(X):
+    """ gram矩阵中，元素x(i,j)就是通道i和通道j的相关性"""
+    num_channels, n = X.shape[1], X.numel() // X.shape[1]
+    X = X.reshape(num_channels, n)
+    return torch.matmul(X, X.T) / (num_channels * n)  # gram矩阵
+    # /(num_channels*n)也就是/X.numel() = 元素总数c*h*w，这样每次计算gram矩阵都不会因为输入图像的尺寸不同而产生变化
+
+def style_loss(Y_hat, gram_Y):
+    return torch.square(Y_hat - gram_Y.detach()).mean()
+    # .mean()得到一个标量
+
+def tv_loss(Y_hat):
+    """ 全变分损失降低噪声 total variation denoising"""
+    return 0.5 * (torch.abs(Y_hat[:,:,1:,:] - Y_hat[:,:,:-1,:]) + torch.abs(Y_hat[:,:,:,1:] - Y_hat[:,:,:,:-1]))
+    # 求和|X(i,j)-X(i,j+1)|+|X(i,j)-X(i+1,j)|
+
+""" 损失函数 """
+content_weight ,style_weight, tv_weight = 1, 1e3, 10 # 使用时广播
+def compute_loss(X, contents_Y_hat, styles_Y_hat, contents_Y, styles_Y_gram):
+    """ 计算内容损失、风格损失、全变分损失"""
+    # 是多层特征，需要遍历每一层计算
+    contents_l = [content_loss(Y_hat, Y)*content_weight for Y_hat, Y in zip(contents_Y_hat, contents_Y)]
+    styles_l = [style_loss(Y_hat, Y) * style_weight for Y_hat, Y in zip(styles_Y_hat, styles_Y_gram)]
+    tv_l = tv_loss(X) * tv_weight
+    l = sum(10 * styles_l + contents_l + [tv_l]) # tv_l 是标量
+    return contents_l, styles_l, tv_l, l
+
+""" 初始化合成图像 """
+class SynthesisedImage(nn.Module):
+    def __init__(self, image_shape, **kwargs):
+        super().__init__(**kwargs)
+        self.weight = nn.Parameter(torch.rand(image_shape))
+
+    def forward(self):
+        return self.weight
+
+def get_init(X, device, lr, styles_Y):
+    gen_image = SynthesisedImage(X.shape).to(device)
+    gen_image.weight.data.copy_(X.data)
+    trainer = torch.optim.Adam(gen_image.parameters(), lr=lr)
+    styles_Y_gram = [gram(Y) for Y in styles_Y]
+    # gram是对单独的一个图求的(每个Y都代表一个不同风格特征)，所以要单独对每个Y求gram
+    return gen_image, trainer, styles_Y_gram
+
+""" 训练 """
+def train(X, contents_Y, styles_Y, device, lr, lr_decay_epoch, epochs):
+    X, trainer, styles_Y_gram = get_init(X, device, lr, styles_Y)
+    scheduler = torch.optim.lr_scheduler.StepLR(trainer, lr_decay_epoch, 0.8)
+    # 经过lr_decay_epoch就更新学习率lr, 新lr=旧lr * 0.8
+
+    for epoch in range(epochs):
+        trainer.zero_grad()
+        contents_Y_hat, styles_Y_hat = extract_features(X, content_layers, style_layers)
+        contents_l, styles_l, tv_l, l = compute_loss(X,contents_Y_hat, styles_Y_hat, contents_Y, styles_Y_gram)
+        l.backward()
+        trainer.step()
+        scheduler.step()
+
+    return X
 
 
